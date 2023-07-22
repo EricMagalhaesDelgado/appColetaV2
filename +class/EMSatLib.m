57fd26ea6e362ddb064b971db47fbd56d23b32a1
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2023-07-21 19:31:53 -0500

    EMSat
    Inclusão no modo de inclusão de testes no comutador e ACU, além do apontamento.

--- +class/EMSatLib.m
+++ +class/EMSatLib.m
@@ -1,7 +1,7 @@
 classdef EMSatLib
 
     % Author.: Eric Magalhães Delgado & Vinicius Puga
-    % Date...: July 4, 2023
+    % Date...: July 21, 2023
     % Version: 1.00
 
     properties
@@ -9,6 +9,7 @@ classdef EMSatLib
         switchSocket
         Antenna
         LNB
+        GeneratedDate
     end
 
 
@@ -41,31 +42,38 @@ classdef EMSatLib
             obj.switchSocket  = tempStruct.Switch;
             obj.Antenna       = tempStruct.Antenna;
             obj.LNB           = tempStruct.LNB;
+            obj.GeneratedDate = tempStruct.GeneratedDate;
         end
 
 
         %-----------------------------------------------------------------%
-        function msgError = PositionValueChanged(obj, Antenna, Type)
+        function msgError = AntennaPositionSET(obj, Antenna, Type)
             msgError = '';
 
-            idx  = find(strcmp({obj.Antenna.Name}, Antenna.Name), 1);
-            IP   = obj.Antenna(idx).IP;
-            Port = obj.Antenna(idx).Port;
+            idx1  = find(strcmp({obj.Antenna.Name}, Antenna.Name), 1);
+            IP   = obj.Antenna(idx1).IP;
+            Port = obj.Antenna(idx1).Port;
 
             try
                 hACU = SocketCreation(obj, IP, Port);
 
                 switch Type
                     case 'Target'
-                        writeline(hACU, sprintf('TT %s T', Antenna.TargetID));
+                        % Syntax: "TT 01 T"
+
+                        idx2 = find({obj.Antenna(idx1).Target.Name}, Antenna.TargetID, 1);
+                        TargetID = obj.Antenna(idx1).Target(idx2).ID;
+
+                        writeline(hACU, sprintf('TT %s T', TargetID));
 
                     case 'LookAngles'
-                        % 'IA AAAAA EEEEE PPP.P'
-                        % Range: IA AAAAA EEEEE PPPP
-                        % Azimuth: 0.00 to 360.00
-                        % Elevation: -180.00 to 180.00
-                        % Polarization: -180.0 to 180.0
-                        writeline(hACU, sprintf('IA %s', Antenna.TargetPosition))
+                        % Syntax: "IA AAA.AAA EEE.EEE +-PPP.PPP"
+                        % Range:
+                        % - Azimuth:         0.00 to 360.00
+                        % - Elevation:    -180.00 to 180.00
+                        % - Polarization: -180.00 to 180.00
+
+                        writeline(hACU, sprintf('IA %.3f %.3f %.3f', wrapTo360(Antenna.Azimuth), wrapTo180(Antenna.Elevation), wrapTo180(Antenna.Polarization)))
                 end
                 pause(class.Constants.antACUPause)
 
@@ -79,11 +87,11 @@ classdef EMSatLib
 
 
         %-----------------------------------------------------------------%
-        function [pos, msgError] = PositionValueChanging(obj, Antenna)
+        function [pos, msgError] = AntennaPositionGET(obj, antennaName)
             pos = [];
             msgError = '';
 
-            idx  = find(strcmp({obj.Antenna.Name}, Antenna.Name), 1);
+            idx  = find(strcmp({obj.Antenna.Name}, antennaName), 1);
             IP   = obj.Antenna(idx).IP;
             Port = obj.Antenna(idx).Port;
 
@@ -99,9 +107,7 @@ classdef EMSatLib
                     
                     pos.Azimuth      = str2double(pos.Azimuth);
                     pos.Elevation    = str2double(pos.Elevation);
-                    pos.Polarization = str2double(pos.Polarization);
-                    if pos.Polarization < 0; pos.Polarization = 360 + pos.Polarization;
-                    end
+                    pos.Polarization = wrapTo360(str2double(pos.Polarization));
                 end
 
             catch  ME
@@ -142,7 +148,7 @@ classdef EMSatLib
 
 
         %-----------------------------------------------------------------%
-        function ConfigFile_Update(obj, RootFolder)
+        function TargetListUpdate(obj, RootFolder)
             PAUSE = 1;
 
             % Switch
@@ -161,6 +167,10 @@ classdef EMSatLib
                 IP   = antStruct(ii).IP;
                 Port = antStruct(ii).Port;
 
+                if isempty(IP)
+                    continue
+                end
+
                 try
                     hACU = SocketCreation(obj, IP, Port);
 
@@ -174,34 +184,31 @@ classdef EMSatLib
                     writeline(hACU, '/ TRACKING TRACK LS')
                     pause(PAUSE)
                     
-                    tgtList = regexp(read(hACU, hACU.NumBytesAvailable, 'char'), '\d{1,2} X (?<ID>T\d{2}) "(?<Name>.*)"', 'names', 'dotexceptnewline');
+                    tgtList = regexp(read(hACU, hACU.NumBytesAvailable, 'char'), '\d{1,2} X T(?<ID>\d{2}) "(?<Name>.*)"', 'names', 'dotexceptnewline');
                     if ~isempty(tgtList)
                         tgtList(deblank({tgtList.Name}) == "") = [];
                         
                         antStruct(ii).Target = struct('ID', {}, 'Name', {}, 'Azimuth', {}, 'Elevation', {}, 'Polarization', {});
                         for jj = 1:numel(tgtList)
-                            writeline(hACU, sprintf('TT %s', extractAfter(tgtList(jj).ID, 'T')))
+                            writeline(hACU, sprintf('TT %s', tgtList(jj).ID))
                             pause(PAUSE)
 
                             tgtInfo = regexp(read(hACU, hACU.NumBytesAvailable, 'char'), '\d{2} \d{2} \d{4} (?<Azimuth>\d{6}) (?<Elevation>\d{6}) (?<Polarization>\d{6})', 'names');
                             if ~isempty(tgtInfo)
                                 tgtInfo = tgtInfo(end);
                                 
-                                tgtInfo.Azimuth      = str2double(tgtInfo.Azimuth)      / 1000;
-                                tgtInfo.Elevation    = str2double(tgtInfo.Elevation)    / 1000;
-                                tgtInfo.Polarization = str2double(tgtInfo.Polarization) / 1000;
-                                if tgtInfo.Polarization < 0; tgtInfo.Polarization = 360+tgtInfo.Polarization;
-                                end
+                                tgtInfo.Azimuth      = str2double(tgtInfo.Azimuth)   / 1000;
+                                tgtInfo.Elevation    = str2double(tgtInfo.Elevation) / 1000;
+                                tgtInfo.Polarization = wrapTo360(str2double(tgtInfo.Polarization) / 1000);
                                 
                                 antStruct(ii).Target(end+1) = struct('ID',           tgtList(jj).ID,    ...
-                                                                   'Name',         tgtList(jj).Name,  ...
-                                                                   'Azimuth',      tgtInfo.Azimuth,   ...
-                                                                   'Elevation',    tgtInfo.Elevation, ...
-                                                                   'Polarization', tgtInfo.Polarization);
+                                                                     'Name',         tgtList(jj).Name,  ...
+                                                                     'Azimuth',      tgtInfo.Azimuth,   ...
+                                                                     'Elevation',    tgtInfo.Elevation, ...
+                                                                     'Polarization', tgtInfo.Polarization);
                             end
                         end
                     end
-
                     clear hACU
 
                 catch ME
@@ -242,7 +249,33 @@ classdef EMSatLib
                                 'MKA-1 HI_L',     19200000000, 0, 24};
 
             % JSON file
-            writematrix(jsonencode(struct('Switch', switchStruct', 'Antenna', antStruct, 'LNB', lnbTable), 'PrettyPrint', true), fullfile(RootFolder, 'Settings', 'EMSatLib.json'), "FileType", "text", "QuoteStrings", false)
+            writematrix(jsonencode(struct('Switch', switchStruct', 'Antenna', antStruct, 'LNB', lnbTable, 'GeneratedDate', datestr(now, 'dd/mm/yyyy HH:MM:SS')), 'PrettyPrint', true), fullfile(RootFolder, 'Settings', 'EMSatLib.json'), "FileType", "text", "QuoteStrings", "none")
+        end
+
+
+        %-----------------------------------------------------------------%
+        function [propTable, propSummary] = TargetProperties(obj)
+
+            % Em 21/07/2023 executei essa função, identificando os seguintes
+            % limites:
+            % (a) Azimuth:     0.118 a 356.300 graus (limites em auxApp.winAddTask: 0 a 360 graus)
+            % (b) Elevation:  15.397 a  63.306 graus (limites em auxApp.winAddTask: 0 a  90 graus)
+            % (c) Polarização: 0.200 a 355.700 graus (limites em auxApp.winAddTask: 0 a 360 graus)
+
+            propTable = table('Size', [0,5], ...
+                                    'VariableTypes', {'cell', 'cell', 'double', 'double', 'double'}, ...
+                                    'VariableNames', {'antenna', 'target', 'azimuth', 'elevation', 'polarization'});
+            
+            for ii = 1:numel(obj.Antenna)
+                for jj = 1:numel(obj.Antenna(ii).Target)
+                    propTable(end+1,:) = {obj.Antenna(ii).Name, ...
+                                                obj.Antenna(ii).Target(jj).Name, ...
+                                                obj.Antenna(ii).Target(jj).Azimuth, ...
+                                                obj.Antenna(ii).Target(jj).Elevation, ...
+                                                obj.Antenna(ii).Target(jj).Polarization};
+                end
+            end
+            propSummary = summary(propTable);
         end
     end
 
